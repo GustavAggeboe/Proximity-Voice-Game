b8575df34009c0179fdfb24b4a9be0afb30ebd96
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-03-04 11:31:11 +0100

    Initial Commit

---
+++ proximity_voice/code/CameraController.cs
@@ -0,0 +1,68 @@
+using Sandbox;
+
+public sealed class CameraController : Component
+{
+	[Property] private PlayerMovement player;
+	[Property] private GameObject head;
+	[Property] private GameObject body;
+	[Property] private float distance;
+
+	private bool isFirstPerson => distance == 0f;
+	private Vector3 currentOffset = Vector3.Zero;
+	private CameraComponent camera;
+	private ModelRenderer bodyRenderer;
+
+	protected override void OnAwake()
+	{
+		if ( IsProxy )
+			Destroy();
+
+		camera = Components.Get<CameraComponent>();
+		bodyRenderer = body.Components.Get<ModelRenderer>();
+	}
+
+	protected override void OnUpdate()
+	{
+		Angles eyeAngles = head.Transform.Rotation.Angles();
+		eyeAngles.pitch += Input.MouseDelta.y * 0.1f;
+		eyeAngles.yaw -= Input.MouseDelta.x * 0.1f;
+		eyeAngles.roll = 0f;
+		eyeAngles.pitch = eyeAngles.pitch.Clamp(-89.9f, 89.9f);
+		head.Transform.Rotation = eyeAngles.ToRotation();
+
+		Vector3 targetOffset = Vector3.Zero;
+		if (player.isCrouching)
+		{
+			targetOffset += Vector3.Down * 26.5f;
+		}
+		currentOffset = Vector3.Lerp( currentOffset, targetOffset, Time.Delta * 10f );
+
+		if (camera != null)
+		{
+			Vector3 position = head.Transform.Position + currentOffset;
+			if (!isFirstPerson)
+			{
+				Vector3 forward = eyeAngles.ToRotation().Forward;
+				SceneTraceResult trace = Scene.Trace.Ray( position, position - (forward * distance) )
+					.WithoutTags( "player", "trigger" )
+					.Run();
+
+				if (trace.Hit)
+				{
+					position = trace.HitPosition + trace.Normal;
+				} else
+				{
+					position = trace.EndPosition;
+				}
+
+				bodyRenderer.RenderType = ModelRenderer.ShadowRenderType.On;
+			} else
+			{
+				bodyRenderer.RenderType = ModelRenderer.ShadowRenderType.ShadowsOnly;
+			}
+
+			camera.Transform.Position = position;
+			camera.Transform.Rotation = eyeAngles.ToRotation();
+		}
+	}
+}
---
+++ proximity_voice/code/PlayerMovement.cs
@@ -0,0 +1,144 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+public sealed class PlayerMovement : Component
+{
+	[Property] private float groundControl = 4.0f;
+	[Property] private float airControl = .1f;
+	[Property] private float maxForce = 10f;
+	[Property] private float speed = 125f;
+	[Property] private float runSpeed = 215f;
+	[Property] private float crouchSpeed = 90f;
+	[Property] private float jumpForce = 200f;
+	[Property] private float standHeight = 69.91f;
+	[Property] private float crouchHeight = 43.5f;
+
+	[Property] private GameObject head;
+	[Property] private GameObject body;
+
+	private Vector3 targetVelocity;
+	public bool isCrouching { get; private set; }
+	private bool isSprinting;
+	private CharacterController characterController;
+	private CitizenAnimationHelper animationHelper;
+
+	[Property] private float velocity;
+
+	protected override void OnAwake()
+	{
+		characterController = Components.Get<CharacterController>();
+		animationHelper = Components.GetInChildrenOrSelf<CitizenAnimationHelper>();
+	}
+	
+	protected override void OnUpdate()
+	{
+		if ( IsProxy )
+			return;
+
+		velocity = characterController.Velocity.Length;
+		HandleStates();
+		UpdateAnimation();
+		Rotate();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if ( IsProxy )
+			return;
+
+		UpdateTargetVelocity();
+		Move();
+	}
+
+	private void UpdateTargetVelocity()
+	{
+		targetVelocity = Input.AnalogMove * head.Transform.Rotation;
+		targetVelocity = targetVelocity.WithZ( 0 );
+		targetVelocity = targetVelocity.IsNearZeroLength ? 0 : targetVelocity.Normal;
+		targetVelocity *= isCrouching ? crouchSpeed : isSprinting ? runSpeed : speed;
+	}
+
+	private void Move()
+	{
+		Vector3 gravity = Scene.PhysicsWorld.Gravity;
+		if (characterController.IsOnGround)
+		{
+			// Friction / acceleration
+			characterController.Velocity = characterController.Velocity.WithZ(0);
+			characterController.Accelerate( targetVelocity );
+			characterController.ApplyFriction( groundControl );
+		} else
+		{
+			// Air control / gravity
+			characterController.Velocity += gravity * Time.Delta * 0.5f;
+			characterController.Accelerate( targetVelocity.ClampLength(maxForce) );
+			characterController.ApplyFriction( airControl );
+		}
+
+		characterController.Move();
+
+		if (!characterController.IsOnGround)
+		{
+			characterController.Velocity += gravity * Time.Delta * 0.5f;
+		} else
+		{
+			characterController.Velocity = characterController.Velocity.WithZ( 0 );
+		}
+	}
+
+	private void Rotate()
+	{
+		if ( body == null )
+			return;
+
+		Angles targetAngle = new Angles( 0, head.Transform.Rotation.Yaw(), 0 ).ToRotation();
+		float rotationSpeed = characterController.Velocity.Length > 0.1f ? Time.Delta * 10f : Time.Delta * 5f;
+		body.Transform.Rotation = Rotation.Lerp( body.Transform.Rotation, targetAngle, rotationSpeed );
+
+		float rotateDifference = body.Transform.Rotation.Distance( targetAngle );
+		if ( rotateDifference > 1f )
+		{
+			animationHelper.FootShuffle = 10f;
+		}
+		
+	}
+
+	private void Jump()
+	{
+		if (!characterController.IsOnGround) { return; }
+
+		characterController.Punch( Vector3.Up * jumpForce );
+		animationHelper?.TriggerJump();
+	}
+
+	private void UpdateAnimation()
+	{
+		if (animationHelper == null) { return; }
+
+		animationHelper.WithWishVelocity( targetVelocity );
+		animationHelper.WithVelocity(characterController.Velocity );
+		animationHelper.AimAngle = head.Transform.Rotation;
+		animationHelper.IsGrounded = characterController.IsOnGround;
+		animationHelper.WithLook( head.Transform.Rotation.Forward, 1f, 0.75f, 0.5f );
+		animationHelper.MoveStyle = CitizenAnimationHelper.MoveStyles.Run;
+		animationHelper.DuckLevel = isCrouching ? 1f : 0f;
+	}
+
+	private void HandleStates()
+	{
+		isCrouching = Input.Pressed( "Crouch" ) ? !isCrouching : isCrouching;
+		isSprinting = Input.Pressed( "Run" ) ? !isSprinting : isSprinting;
+		if ( !Input.Down( "Forward" ) || velocity <= 110f )
+		{
+			isSprinting = false;
+		}
+
+		if ( Input.Pressed( "Jump" ) )
+		{
+			Jump();
+			isCrouching = false;
+		}
+
+		characterController.Height = isCrouching ? crouchHeight : standHeight;
+	}
+}
